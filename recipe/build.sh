e24dbb4e81018ebbe3a637c6cbf5d3f5e571b7d4
Julien Lamy <lamy@unistra.fr>
2021-06-10 18:28:34 +0200

    Re-add windows builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,13 +6,10 @@ set -e
 mkdir build
 cd build
 
-export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
-
 cmake \
-  -GNinja \
   -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF \
-  -DBUILD_PYTHON_WRAPPERS=ON -DUSE_OPENMP=OFF \
+  -DBUILD_PYTHON_WRAPPERS=ON \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPYTHON_EXECUTABLE=${PYTHON} \
   ../
 
-cmake --build . --target install
+cmake --build . --config Release --target install
